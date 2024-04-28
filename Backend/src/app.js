3d1fe2db2de4130f34bbba2d09b8299b3f996139
Shivam Singh Yadav <136052614+Shivam-ssy@users.noreply.github.com>
2024-04-28 11:52:58 +0530

    setting static folder

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 })); 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-
+app.use(express.static('uploads'));
 app.use(cookieParser())
 
 
