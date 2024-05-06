c2ad146cc3f6fb8ca3d7cd86c7a7d75d8ed142e5
Shivam Singh Yadav <shivamsinghyadavssy888@gmail.com>
2024-05-06 14:33:40 +0530

    error debugging5

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,8 +1,10 @@
 import multer from "multer";
-
+import path from "path"
+const tempPath=path.resolve("./uploads")
+console.log(tempPath)
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./uploads")
+      cb(null, tempPath)
     },
     filename: function (req, file, cb) {
       
