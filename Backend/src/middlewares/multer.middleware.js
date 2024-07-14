32ca4971745c59adbebac79b46e2a49e0a44c066
shivam Singh Yadav <shivamsinghyadavssy888@gmail.com>
2024-07-14 12:00:01 +0530

    trying to fix multer

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -12,7 +12,7 @@ try {
 }
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, tempPath)
+      cb(null, `${tempPath}`)
     },
     filename: function (req, file, cb) {
       
